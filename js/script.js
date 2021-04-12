c11d334cbb254693cd9789aca52976152ae02f94
stellarvoid <danya.danya.lool@gmail.com>
2021-04-12 18:45:09 +0300

    Почти готово

--- js/script.js
+++ js/script.js
@@ -3,7 +3,25 @@ $(document).ready(function () {
 		$('.responsive-menu').toggleClass('active');
 		$('.burger').toggleClass('active-burger');
 		$('body,.container-header').toggleClass('blure');
-		$('.title-block-img-1').toggleClass('none');
+		$('.title-block-img-1,img,.map').toggleClass('none');
+		$('body').toggleClass('overflow');
+	});
+
+	$('.button-more').click(function (event) {
+		$('.services-items').toggleClass('services-active');
+		$('.arrow-more').toggleClass('arrow-active')
 	});
 });
+$('.nav,.nav-r a').on('click', function () {
 
+	let href = $(this).attr('href');
+
+	$('html, body').animate({
+		scrollTop: $(href).offset().top
+	}, {
+		duration: 570,   // по умолчанию «400» 
+		easing: "linear" // по умолчанию «swing» 
+	});
+
+	return false;
+});
